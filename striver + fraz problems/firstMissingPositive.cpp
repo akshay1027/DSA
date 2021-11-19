c9d63bf982a4ae8fb74a769d783e49826e096e0f
Akshay R R <akshayar1027@gmail.com>
2021-11-20 00:48:44 +0530

    LC hard

--- striver + fraz problems/firstMissingPositive.cpp
+++ striver + fraz problems/firstMissingPositive.cpp
@@ -1,27 +1,32 @@
+// Put each number in its right place.
+// For example:
+
+// When we find 5, then swap it with A[4].
+// At last, the first place where its number is not right, return the place + 1.
+
 class Solution
 {
 public:
     int firstMissingPositive(vector<int> &nums)
     {
-        int result;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] < nums.size())
-            {
-                int temp = nums[i];
-                swap(nums[temp], nums[i]);
-            }
-        }
-        for (int i = 0; i < nums.size(); i++)
+        int n = nums.size();
 
+        for (int i = 0; i < n; i++)
         {
-            if ((nums[i] == i + 1) && (nums[i] > 0))
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
             {
-                result = i + 1;
-                break;
+                swap(nums[i], nums[nums[i] - 1]);
             }
         }
 
-        return result;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != i + 1)
+            {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
     }
 };
